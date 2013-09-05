6b46ab84446c3fd22f7b19e2b8f7024eb860fcc2
Kobi Gurkan <kg@kgrnd.kgrnd>
2013-09-05 22:33:25 +0300

    batch point location queries

--- PointColorCalculator.hpp
+++ PointColorCalculator.hpp
@@ -10,12 +10,15 @@
 #include <QGraphicsView>
 #include <QPixmap>
 #include "ColoredPointsGraphicsItem.hpp"
+#include <CGAL/Arr_batched_point_location.h>
 
 enum PointColorCalculatorAlgorithm
 {
 	OurAlgorithm = 0,
 	NN = 1
 };
+
+typedef std::pair<Point_2, CGAL::Object>              Query_result;
 
 class PointColorCalculator
 {
@@ -58,117 +61,74 @@ class PointColorCalculator
 		CGAL::insert (arr, &cv[0], &cv[points_only.size()]);
 	}	
 
-	int GetColor(Point_2 p)
+
+	std::list<std::pair<Point_2,int> > GetColors(std::list<Point_2> points)
 	{
-		point_location_query_type	point_locator(arr);
-		CGAL::Object obj = point_locator.locate(p);
+		std::list<std::pair<Point_2,int> > result_points; 
+		std::list<Query_result> results;
+		locate(arr, points.begin(), points.end(), std::back_inserter(results));
 		Arrangement_2::Face_const_handle f;
 		Arrangement_2::Halfedge_const_handle he;
-		Colored_segment *s;
-		CGAL::set_pretty_mode(std::cout);
 		Traits_2::Compute_squared_distance_2 squared_distance;
-		Number_type score[MAX_COLOR];
-		int chosen_color = -2;
-		if (CGAL::assign(f,obj) && !f->is_unbounded())
+		std::list<Query_result>::const_iterator  res_iter;
+		for (res_iter = results.begin(); res_iter != results.end(); ++res_iter)
 		{
-			chosen_color = -1; //we're in face, but it shouldn't be used
-			if (!f->has_outer_ccb()) return chosen_color;
-			Number_type max_score = 0;
-			for (int i = 0; i < MAX_COLOR; i++) 
+			Colored_segment *s;
+			Number_type score[MAX_COLOR];
+			int chosen_color = -2;
+			if (CGAL::assign(f,res_iter->second) && !f->is_unbounded())
 			{
-				score[i] = 0;
-				Arrangement_2::Ccb_halfedge_const_circulator first, curr;
-				curr = first = f->outer_ccb();
-				do {
-					he = curr;
-					curr++;
-					s = (Colored_segment*)&he->curve();;
-					int data_size = he->curve().data().size();
-					if (data_size != 1) {
-					}
-					Data_traits::Data_container::const_iterator it;
-					int color = he->curve().data().front();
-					if (color == i)
-					{	
-						if (this->algorithm == OurAlgorithm)
-						{
-							Point_2 proj = s->line().projection(p);
-							Kernel::Segment_2 segment(s->source(),s->target());
-							if (!segment.has_on(proj)) continue;
-							Number_type distance_from_projection = squared_distance(p,proj);
-							Number_type distance_from_edges = std::min(squared_distance(proj,he->source()->point()),squared_distance(proj,he->target()->point())); 
-							Number_type score_from_this_line = distance_from_edges / distance_from_projection;
-							score[i] += score_from_this_line;
-						}
-						if (this->algorithm == NN)
-						{
-							Point_2 proj = s->line().projection(p);
-							Kernel::Segment_2 segment(s->source(),s->target());
-							if (!segment.has_on(proj)) continue;
-							Number_type distance_from_projection = squared_distance(p,proj);
-							score[i] = score[i] == 0 ? 1/distance_from_projection: std::max(1/distance_from_projection,score[i]);
-						}
-					}
-				} while (curr != first);
-				double normalized_score = CGAL::to_double(score[i]);
-				if (normalized_score > max_score) 
+				Point_2 p = res_iter->first; 
+				chosen_color = -1; //we're in face, but it shouldn't be used
+				if (!f->has_outer_ccb()) continue;
+				Number_type max_score = 0;
+				for (int i = 0; i < MAX_COLOR; i++) 
 				{
-					max_score = normalized_score;
-					chosen_color = i;
-				}	
+						score[i] = 0;
+						Arrangement_2::Ccb_halfedge_const_circulator first, curr;
+						curr = first = f->outer_ccb();
+						do {
+							he = curr;
+							curr++;
+							s = (Colored_segment*)&he->curve();;
+							int data_size = he->curve().data().size();
+							if (data_size != 1) {
+							}
+							Data_traits::Data_container::const_iterator it;
+							int color = he->curve().data().front();
+							if (color == i)
+							{	
+								if (this->algorithm == OurAlgorithm)
+								{
+									Point_2 proj = s->line().projection(p);
+									Kernel::Segment_2 segment(s->source(),s->target());
+									if (!segment.has_on(proj)) continue;
+									Number_type distance_from_projection = squared_distance(p,proj);
+									Number_type distance_from_edges = std::min(squared_distance(proj,he->source()->point()),squared_distance(proj,he->target()->point())); 
+									Number_type score_from_this_line = distance_from_edges / distance_from_projection;
+									score[i] += score_from_this_line;
+								}
+								if (this->algorithm == NN)
+								{
+									Point_2 proj = s->line().projection(p);
+									Kernel::Segment_2 segment(s->source(),s->target());
+									if (!segment.has_on(proj)) continue;
+									Number_type distance_from_projection = squared_distance(p,proj);
+									score[i] = score[i] == 0 ? 1/distance_from_projection: std::max(1/distance_from_projection,score[i]);
+								}
+							}
+						} while (curr != first);
+						double normalized_score = CGAL::to_double(score[i]);
+						if (normalized_score > max_score) 
+						{
+							max_score = normalized_score;
+							chosen_color = i;
+						}	
+				}
+				result_points.push_back(std::pair<Point_2,int>(p, chosen_color));
 			}
 		}
-		return chosen_color;
-	}
-
-	void to_geomview(int resolution_x, int resolution_y)
-	{
-		int color = -1;
-		Number_type min_x = rect.xmin();
-		Number_type min_y = rect.ymin();
-		Number_type  max_x = rect.xmax();
-		Number_type  max_y = rect.ymax();	
-		
-		gv = new CGAL::Geomview_stream(CGAL::Bbox_3(CGAL::to_double(min_x),CGAL::to_double(min_y),0,CGAL::to_double(max_x),CGAL::to_double(max_y),0));
-		(*gv).set_line_width(4);
-		(*gv).clear();
-		for (std::list<std::pair<Segment_2,int> >::iterator it = segments.begin(); it != segments.end();it++)
-		{
-			static int color_rgb[3];
-			resolve_int_to_color((*it).second,color_rgb);
-			*gv << CGAL::Color(color_rgb[0],color_rgb[1],color_rgb[2]);
-			*gv << (*it).first;
-		}
-		std::cout << "min_x,max_x,min_y,max_y: " << min_x << "," << max_x << "," << min_y << "," << max_y << std::endl;
-		Number_type step_x = (max_x-min_x)/resolution_x;
-		Number_type step_y = (max_y-min_y)/resolution_y;
-		Point_2 *test_point;
-		Point_3 *draw_point;
-		int counter = 0;
-		for ( Number_type i = min_y;i<=max_y;i+=step_y)
-		{
-			for (Number_type j = min_x; j<=max_x;j+=step_x)
-			{
-				if (counter++ % 1000 == 0)
-					std::cout << "for point " << i << "," << j << " got color " << (color >= 0 ? color_names[color] : "UNKNOWN") << std::endl;
-
-				test_point = new Traits_2::Point_2(j,i);
-				color = GetColor(*test_point);	
-				static int color_rgb[3];
-				resolve_int_to_color(color,color_rgb);
-				if (color_rgb[0]==-1) continue;
-				delete test_point;
-				draw_point = new Traits_2::Point_3(j,i,0);
-				*gv << CGAL::Color(color_rgb[0],color_rgb[1],color_rgb[2]);
-				*gv << *draw_point;
-				delete draw_point;
-			}
-		}
-		char ch = 'a';
-		while (ch != 'y') { 
-			std::cout << "Press y to finish";
-			std::cin >> ch;
-		}
+		return result_points;
 	}
 
 	int to_qt(std::string file_name, int resolution_x, int resolution_y)
@@ -209,25 +169,26 @@ class PointColorCalculator
 			q_lines.push_back(std::pair<QLineF,QColor>(QLineF(source,target),QColor(color_rgb[0],color_rgb[1],color_rgb[2])));
 		}
 		std::cout << "min_x,max_x,min_y,max_y: " << min_x << "," << max_x << "," << min_y << "," << max_y << std::endl;
-		Point_2 *test_point;
-		int counter = 0;
+		std::list<Point_2> points;
 		std::list<std::pair<QPointF,QColor> > q_points;
 		for ( Number_type i = min_y;i<=max_y;i+=step_y)
 		{
 			for (Number_type j = min_x; j<=max_x;j+=step_x)
 			{
-				if (counter++ % 1000 == 0)
-					std::cout << "for point " << i << "," << j << " got color " << (color >= 0 ? color_names[color] : "UNKNOWN") << std::endl;
-
-				test_point = new Traits_2::Point_2(j,i);
-				color = GetColor(*test_point);	
-				static int color_rgb[3];
-				resolve_int_to_color(color,color_rgb);
-				if (color_rgb[0]==-1) continue;
-				QColor color(color_rgb[0],color_rgb[1],color_rgb[2]);
-				q_points.push_back(std::pair<QPointF,QColor>(QPointF(CGAL::to_double(test_point->x()),CGAL::to_double(test_point->y())),QColor(color_rgb[0],color_rgb[1],color_rgb[2])));
-				delete test_point;
+				points.push_back(Point_2(j,i));
 			}
+		}
+		
+		std::list<std::pair<Point_2,int> > result_points = GetColors(points);
+
+		std::cout << "got color" << std::endl;
+		for (std::list<std::pair<Point_2, int> >::iterator iter = result_points.begin(); iter != result_points.end(); ++iter)
+		{
+			static int color_rgb[3];
+			resolve_int_to_color(iter->second,color_rgb);
+			if (color_rgb[0]==-1) continue;
+			QColor color(color_rgb[0],color_rgb[1],color_rgb[2]);
+			q_points.push_back(std::pair<QPointF,QColor>(QPointF(CGAL::to_double((*iter).first.x()),CGAL::to_double((*iter).first.y())),QColor(color_rgb[0],color_rgb[1],color_rgb[2])));
 		}
 		ColoredPointsGraphicsItem q_pointsItem(&q_lines, &q_points, &rect);
 		scene.addItem(&q_pointsItem);
